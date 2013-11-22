8b2ba49c022e0ce71a67e72b1c99c298c2d14e5b
doCooler wonderlai <yi01625@163.com>
2013-11-22 21:28:57 +0800

    fisrt commit

---
+++ bubble_sort.go
@@ -0,0 +1,31 @@
+package main 
+
+import "fmt"
+
+func bubble_sort(array []int) {
+	length := len(array)
+	for i:=0; i<length;i++{
+		for j := i+1; j < length; j++ {
+			if array[j] < array[i] {
+				array[j],array[i] = array[i],array[j]
+			}
+		}
+	}
+}
+
+func main() {
+	arr := [] int{4, 88, 5, 21, 92, 37, 56, 13, 75, 19, 64, 57, 94, 34, 8, 12, 71, 99, 102, 38}
+	fmt.Println("before:")
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("%d ", arr[i])
+	}
+    fmt.Println("")
+	
+	bubble_sort(arr)
+
+	fmt.Println("after:")
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("%d ", arr[i])
+	}
+	fmt.Println("done")
+}
---
+++ bubble_sort.py
@@ -0,0 +1,17 @@
+def bubble_sort(data):
+	length = len(data)
+	for i in xrange(0, length):
+		for j in xrange(i+1, length):
+			if (data[j] < data[i]):
+				tmp = data[j]
+				data[j] = data[i]
+				data[i] = tmp
+
+def test_func():
+	arr = [4, 88, 5, 21, 92, 37, 56, 13, 75, 19, 64, 57, 94, 34, 8, 12, 71, 99, 102, 38]
+	print "before:"
+	print arr
+	bubble_sort(arr)
+	print "after:"
+	print arr
+test_func()
